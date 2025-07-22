8c3888b6c6e0667576b365a2c04217a2389ff00e
Nary <635683+Nary@users.noreply.github.com>
2025-07-22 14:56:56 +0200

    Add function createOneDiv and other setter

--- js/script.js
+++ js/script.js
@@ -1,20 +1,40 @@
-const startL = 16;
-const startH = 16;
+const starterL = 16;
+const starterH = 16;
 const mainContainer = document.querySelector("#mainContainer")
 
 // mainContainer.textContent = "Hello"
 
-let divL = startL;
-let divH = startH;
-function createDiv() {
+// box-sizing: border-box;
+// width: 100px;
+// height: 100px;
+// background-color: white;
+
+
+let divL = starterL;
+let divH = starterH;
+
+function createElementDiv() {
     let div = document.createElement("div")
     return div;
 }
 
-function setDivColor(div,color) {
+function setDivColor(div, color) {
     div.style.backgroundColor = color;
 }
+function setDivWidth(div, width) {
+    div.style.width = width;
+}
+function setDivHeight(div, height) {
+    div.style.height = height;
+}
 
+function createOneDiv() {
+    let div = createElementDiv();
+    setDivColor(div,"black")
+    setDivHeight(div,"100px")
+    setDivWidth(div,"100px")
+    return div;
+}
 function createAllDiv(l,h) {
     for (i=0;i<l;i++) {
         for (j=0;j<h;j++) {
@@ -24,7 +44,7 @@ function createAllDiv(l,h) {
     }
 }
 
-let maDiv = createDiv();
+let maDiv = createOneDiv();
 setDivColor(maDiv,"red")
 maDiv.textContent = "hiMiniDiv"
 mainContainer.appendChild(maDiv)
