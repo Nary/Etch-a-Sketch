c8d3ab238d93a4914ec3dbe979b0b5397e4746b3
Nary <635683+Nary@users.noreply.github.com>
2025-07-24 11:32:58 +0200

    Finish etch-a-sketch + extra credit RGB + Opacity

--- js/script.js
+++ js/script.js
@@ -1,18 +1,13 @@
 const starterL = 16;
 const starterH = 16;
-const GRAY = "gray"
-const mainEtchContainer = document.querySelector("#mainEtchContainer")
+// const GRAY = "gray";
 
+const mainEtchContainer = document.querySelector("#mainEtchContainer");
+const buttonChange = document.querySelector("#buttonChg");
 const mainEtchHeight = 800;
 const mainEtchWidth = 800;
-// mainContainer.textContent = "Hello"
 
-// box-sizing: border-box;
-// width: 100px;
-// height: 100px;
-// background-color: white;
-
-
+let rgbOpacity = 0;
 let divL = starterL;
 let divH = starterH;
 
@@ -38,42 +33,70 @@ function setDivBorder(div,border) {
 
 function createOneDiv(height,width,color) {
     let div = createElementDiv();
-    let heightPX = height.toString() + "px"
-    let widthPX = width.toString() + "px"
-    setDivColor(div,color)
-    setDivHeight(div,heightPX)
-    setDivWidth(div,widthPX)
-    setDivBorder(div,"2px solid black")
+    let heightPX = height.toString() + "px";
+    let widthPX = width.toString() + "px";
+    setDivColor(div,color);
+    setDivHeight(div,heightPX);
+    setDivWidth(div,widthPX);
+    setDivBorder(div,"2px solid black");
     return div;
-}
-function createAllDiv(l,h) {
-    for (i=0;i<l;i++) {
-        for (j=0;j<h;j++) {
-            //
-            //
-        }
-    }
 }
 
 function hoverListener(ev) {
-    ev.target.style.backgroundColor = GRAY;
+    let newColor = randomColor();
+    ev.target.style.backgroundColor = newColor;
 }
-function initialGrid() {
-    
+
+function initGrid() {
     let width = mainEtchWidth / starterL;
     let height = mainEtchHeight / starterH;
     let div;
-    let color = "white"
+    let color = "white";
     for (i=0;i<starterL;i++) {
         for (j=0;j<starterH;j++) {
-            div = createOneDiv(width,height,color)
-            div.addEventListener("mouseleave",hoverListener)
-            mainEtchContainer.appendChild(div)
+            div = createOneDiv(width,height,color);
+            div.addEventListener("mouseleave",hoverListener);
+            mainEtchContainer.appendChild(div);
         }
     }
 }
-let maDiv = createOneDiv(100,100,"red");
-// setDivColor(maDiv,"red")
-// maDiv.textContent = "hiMiniDiv"
-// mainEtchContainer.appendChild(maDiv)
-initialGrid()
+function randomColor() {
+    let colorRed = Math.floor(Math.random() * 256);
+    let colorGreen = Math.floor(Math.random() * 256);
+    let colorBlue = Math.floor(Math.random() * 256);
+    if (rgbOpacity < 1) {
+        rgbOpacity += 0.1;
+    }
+    return `rgba(${colorRed}, ${colorGreen}, ${colorBlue}, ${rgbOpacity})`;
+}
+function resetGrid(newSize) {
+    
+    while (mainEtchContainer.firstChild) {
+        mainEtchContainer.removeChild(mainEtchContainer.lastChild); // removing lastChild faster than first ?
+    }
+    let width = mainEtchWidth / newSize;
+    let height = mainEtchHeight / newSize;
+    let div;
+    let color = "white";
+    for (i=0;i<newSize;i++) {
+        for (j=0;j<newSize;j++) {
+            div = createOneDiv(width,height,color);
+            div.addEventListener("mouseleave",hoverListener);
+            mainEtchContainer.appendChild(div);
+        }
+    }
+
+}
+
+buttonChange.addEventListener("click", () =>{
+    let input = prompt("Enter the new size");
+    let size = parseInt(input, 10)
+
+    if (isNaN(size) || size > 100 || size < 1) {
+        size = 100; // default max size
+    }
+
+    resetGrid(size);
+})
+
+initGrid();
